5f6954b02e7d4158a4ba5332a00d2030fc32f092
ma10123 <920085956@qq.com>
2019-04-16 23:11:04 +0800

    导入项目

---
+++ loveandpeace/src/main/java/com/mkesai/loveandpeace/LoveandpeaceApplication.java
@@ -0,0 +1,13 @@
+package com.mkesai.loveandpeace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveandpeaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveandpeaceApplication.class, args);
+    }
+
+}
---
+++ loveandpeace/src/test/java/com/mkesai/loveandpeace/LoveandpeaceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mkesai.loveandpeace;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoveandpeaceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
